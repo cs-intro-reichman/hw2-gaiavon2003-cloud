1381506f552b2b42163ddddc971077bb07784990
Gaia von Kauffmann <gaia.vonkauffmann@post.runi.ac.il>
2025-11-09 18:40:03 +0200

    Completed assignment

--- TimeCalc.java
+++ TimeCalc.java
@@ -4,10 +4,10 @@ public class TimeCalc {
         int hours = Integer.parseInt(""+ args[0].charAt(0)+ args[0].charAt(1));
         int minuutes = Integer.parseInt("" +args[0].charAt(3)+ args[0].charAt(4));
         int minuutesToAdd = Integer.parseInt(args[1]);
-        int Totalminutes = ((hours * 60) + minuutes + minuutesToAdd);
-        int Totalhours = Totalminutes/60;
-        int newhours = Totalhours%24;
-        int newMinutes = Totalminutes - (Totalhours*60);
+
+        int Totalminutes = (hours * 60 + minuutes + minuutesToAdd);
+        int newhours = (Totalminutes / 60)%24;
+        int newMinutes = Totalminutes %60;
         System.out.println(newhours + ":" + newMinutes);
 
 
