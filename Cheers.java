8fdb02ee47fc760b35736344e3d4bfccd8ac86ef
Gaia von Kauffmann <gaia.vonkauffmann@post.runi.ac.il>
2025-11-10 15:02:09 +0200

    Completed assignment

--- Cheers.java
+++ Cheers.java
@@ -15,8 +15,8 @@ public class Cheers {
             for ( int i= 0; i< letter; i++) {
                 char bigletter = Character.toUpperCase(name.charAt(i));
                 if (bigletter == 'A'|| bigletter == 'E'|| bigletter == 'F'|| bigletter == 'H'|| bigletter == 'I'|| bigletter == 'L'|| bigletter == 'M'|| bigletter == 'N'|| bigletter == 'O'|| bigletter == 'R'|| bigletter == 'S'|| bigletter == 'X'){
-                   System.out.println("Give me an " +bigletter + ": " + bigletter);     
-                } else {System.out.println("Give me a " +bigletter + ": " + bigletter);
+                   System.out.println("Give me an " +bigletter + ": " + bigletter +"!");     
+                } else {System.out.println("Give me a " +bigletter + ": " + bigletter+"!");
 
                 }
 
