b2b82a9c1d63b22c133c863e6d19d0cd18aaa977
Gaia von Kauffmann <gaia.vonkauffmann@post.runi.ac.il>
2025-11-10 19:06:17 +0200

    completed assignment

--- Collatz.java
+++ Collatz.java
@@ -1,40 +1,35 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
-	int n = Integer.parseInt(args[0]);
-	String mode = args [1];
-	for ( int seed = 1; seed <= n ; seed ++) {
-		int currentNumber = seed; 
-		int steps = 0;
-		
-		if (mode.equals("v")) {
-			System.out.print( currentNumber + " ");
-		}
-		while (currentNumber != 1 ) {
-			if (currentNumber % 2 == 0 ) {
-				currentNumber = currentNumber / 2;
-				
-			} else { 
-				currentNumber = (currentNumber * 3) + 1;
-			}
-			steps++;
+		int n = Integer.parseInt(args[0]);
+		String mode = args[1];
+
+		for (int seed = 1; seed <= n; seed++) {
+			int currentNumber = seed;
+			int steps = 1;
+
 			if (mode.equals("v")) {
-				System.out.print(currentNumber + " ");	
+				System.out.print(currentNumber + " ");
 			}
+
+			do {
+				if (currentNumber % 2 == 0) {
+					currentNumber = currentNumber / 2;
+
+				} else {
+					currentNumber = (currentNumber * 3) + 1;
+				}
+				steps++;
+				if (mode.equals("v")) {
+					System.out.print(currentNumber + " ");
+				}
+			} while (currentNumber != 1);
+			if (mode.equals("v")) {
+				System.err.print("(" + steps + ")");
+				System.out.println();
+			}
+
+		}
+		System.out.println("Every one of the first " + n + " hailstone sequences reached 1.");
 	}
-	if (mode.equals("v")) {
-		System.out.println();
-		System.out.println("Seed "+ seed + "took "+ steps + "steps.");
-		System.out.println("---");
-	} 
-	
-	
-	
-	}
-	System.out.println("Verified the Collatz conjecture for seeds 1 to " + n + ".");
 }
-}
-
-
-
